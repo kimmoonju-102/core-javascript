1e41dd9edeb22894e99dc0d7ea7081ff11ed0c5d
kimmoonju-102 <kimmun3549@naver.com>
2023-01-16 14:36:30 +0900

    feat : 초기환경 설정 및randomDice 만들기

--- client/main.js
+++ client/main.js
@@ -1,2 +1,112 @@
-console.log(window);
+import { 
+  diceAnimation, 
+  disableElement, 
+  enableElement, 
+  getNode, 
+  getNodes,
+  visibleElement,
+  invisibleElement,
+  insertLast
+ } from "./lib/index.js";
 
+
+
+// [ 주사위 굴리기 ]
+// 1. dice 애니메이션 불러오기
+// 2. bindEvent 유틸 함수 만들기 
+// 3. handleRollingDice 함수 만들고 토글로 애니메이션 제어하기 
+// 4. 변수 보호를 위한 클로저 + IIFE 사용하기 
+
+// [ 레코드 리스트 보이기 ]
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만듭니다.
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기 
+
+// [ 레코드 템플릿 뿌리기 ]
+// 1. renderRecordListItem 함수 만들기
+// 2. HTML 템플릿 만들기
+// 3. 템플릿 뿌리기 
+
+// diceAnimation()
+
+// getNodes list가 반환됌 (유사배열)
+// 배열의 구조 분해 할당
+const [rollingDiceButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+
+const recordListWrapper = getNode('.recordListWrapper')
+
+
+
+
+function renderRecordListItem(){
+  let template = /* html */
+  `<tr>
+    <td>0</td>
+    <td>5</td>
+    <td>5</td>
+  </tr>`
+
+  insertLast('.recordListWrapper tbody', template)
+}
+
+/* -------------------------------------------------------------------------- */
+/* event                                                                      */
+/* -------------------------------------------------------------------------- */
+
+// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// const recordButton = getNode(".buttonGroup > button:nth-child(2)");
+// const restButton = getNode(".buttonGroup > button:nth-child(3)");
+
+
+// let stopAnimation;
+
+// IIFE
+// 참고로 즉시실행 함수는 (함수)(), (함수()) 두 패턴으로 사용합니다요
+const handleRollingDice = (() => {
+ 
+  let isRolling = false;
+  let stopAnimation;  
+
+  return () => {
+    if(!isRolling){
+      // console.log('첫번째 클릭');
+      stopAnimation = setInterval(diceAnimation,100)
+      
+      disableElement(recordButton)
+      disableElement(resetButton)
+      
+      
+    }else{
+      // console.log('두번째 클릭');
+      clearInterval(stopAnimation);
+      enableElement(recordButton)
+      enableElement(resetButton)
+    }
+  
+    isRolling = !isRolling;
+  }
+
+})()
+
+
+const handleRecord =()=>{
+  
+  visibleElement(recordListWrapper);
+
+  renderRecordListItem();
+}
+
+const handleReset = () => {
+
+  invisibleElement(recordListWrapper)
+}
+
+
+
+// handlerRollingDice 실행시키면 함수 본문 전체가 실행됌. 그래서 실행실행을 시켜야 함
+rollingDiceButton.addEventListener('click',handleRollingDice)
+recordButton.addEventListener('click',handleRecord)
+resetButton.addEventListener('click',handleReset)
+// let eventOff = bindEvent(rollingDiceButton,'click',handlerRollingDice);
